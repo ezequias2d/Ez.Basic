84c98ff016b021b99c89da6a7babcdd1a18fbd1d
ezequias2d <ezequiasmoises@gmail.com>
2022-04-03 02:30:02 -0300

    Adds parser exception

---
+++ src/Ez.Basic/Compiler/Parser/ParserException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ez.Basic.Compiler.Parser
+{
+    public class ParserException : Exception
+    {
+        public ParserException()
+        {
+        }
+
+        public ParserException(string message) : base(message)
+        {
+        }
+    }
+}
