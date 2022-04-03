baf30b4580cbdc957d408d851ac6b0fc35703e89
ezequias2d <ezequiasmoises@gmail.com>
2022-04-03 02:29:47 -0300

    Implements by hand the parser

--- src/Ez.Basic/Compiler/Parser/BasicParser.cs
+++ src/Ez.Basic/Compiler/Parser/BasicParser.cs
@@ -3,38 +3,66 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static Ez.Basic.Compiler.Parser.Ast;
 
 namespace Ez.Basic.Compiler.Parser
 {
     public ref struct BasicParser
     {
+        private readonly Stack<Ast.Node> m_pool;
         private readonly ILogger Logger;
         private Scanner Scanner;
-        private Token Current;
-        private Token Previous;
+        private Token m_current;
+        private Token m_previous;
         public bool HadError;
 
         public BasicParser(ILogger logger, Scanner scanner)
         {
+            m_pool = new Stack<Ast.Node>();
             Logger = logger;
             Scanner = scanner;
-            Current = Previous = default;
+            m_current = m_previous = default;
             HadError = false;
         }
-        
-        private void Advance()
+
+        public Token Current => m_current;
+        public Token Previous => m_previous;
+
+        public void Advance()
         {
-            Previous = Current;
+            m_previous = Current;
 
             while(true)
             {
-                Current = Scanner.ScanToken();
+                m_current = Scanner.ScanToken();
 
                 if (Current.Type != TokenType.Error)
                     break;
 
                 ErrorAtCurrent(Current.Lexeme.ToString());
             }
+        }
+
+        public void Consume(TokenType type, string message)
+        {
+            if(Current.Type == type)
+            {
+                Advance();
+                return;
+            }
+
+            ErrorAtCurrent(message);
+        }
+
+        public void ConsumeAny(TokenType type1, TokenType type2, string message)
+        {
+            if (Current.Type == type1 || Current.Type == type2)
+            {
+                Advance();
+                return;
+            }
+
+            ErrorAtCurrent(message);
         }
 
         private void ErrorAtCurrent(string message)
@@ -47,7 +75,7 @@ namespace Ez.Basic.Compiler.Parser
             ErrorAt(Previous, message);
         }
 
-        private void ErrorAt(Token token, string message)
+        private ParserException ErrorAt(Token token, string message)
         {
             var sb = new StringBuilder();
 
@@ -64,7 +92,486 @@ namespace Ez.Basic.Compiler.Parser
 
             sb.Append(": ");
             sb.AppendLine(message);
+
+            Logger?.LogError(sb.ToString());
             HadError = true;
+
+            return new ParserException();
+        }
+
+        private void Synchronize()
+        {
+            Advance();
+            //while(!Scanner.IsAtEnd())
+            //{
+            //    switch(Current.Type)
+            //    {
+            //        case TokenType.Module:
+            //        case TokenType.Mould:
+            //        case TokenType.Def:
+            //        case TokenType.Let:
+            //        case TokenType.For:
+            //        case TokenType.If:
+            //        case TokenType.While:
+            //        case TokenType.Print:
+            //        case TokenType.Return:
+            //            return;
+            //    }
+            //    Advance();
+            //}
+        }
+
+        internal Node Statement()
+        {
+            if (Match(TokenType.For)) return ForStatement();
+            if (Match(TokenType.If)) return IfStatement();
+            if (Match(TokenType.Print)) return PrintStatement();
+            if (Match(TokenType.While)) return WhileStatement();
+            if (Match(TokenType.Do)) return Block(TokenType.Next);
+
+            return ExpressionStatement();
+        }
+
+        internal Node ForStatement()
+        {
+            var token = Previous;
+
+            Consume(TokenType.Identifier, "Expect a identifier to interate.");
+            var name = Previous;
+
+            Consume(TokenType.Equal, "Expect a initializer.");
+            var initializer = Expression();
+
+            Consume(TokenType.To, "Expect a limitation");
+            var to = Expression();
+
+            Node step = null;
+            if(Match(TokenType.Step))
+                step = Expression();
+
+            var mainBody = Block(TokenType.Next);
+            Node body = mainBody;
+
+
+            body = new Block(token, mainBody.EndToken, new Node[]
+            {
+                body,
+                step != null 
+                    ? MakeIncrementStatement(step.Token, name, step) 
+                    : MakeIncrementStatement(token, name)
+            });
+
+            // create loop while
+            var condition = MakeLessEqualExpression(to.Token, name, to);
+            body = MakeWhile(token, condition, body);
+
+            // create let declaration and initializer.
+            body = new Block(token, mainBody.EndToken, new Node[]
+            {
+                MakeLet(name, initializer),
+                body
+            });
+
+            return body;
+        }
+
+        internal Node IfStatement()
+        {
+            var token = Previous;
+            var condition = Expression();
+
+            var thenBranch = Block(TokenType.Next, TokenType.Else);
+            Node elseBranch = null;
+
+            if(thenBranch.EndToken.Type == TokenType.Else)
+                elseBranch = Block(TokenType.Next);
+
+            return MakeIf(token, condition, thenBranch, elseBranch);
+        }
+
+        internal Node PrintStatement()
+        {
+            var printToken = Previous;
+            Node expr = Expression();
+            while (Match(TokenType.Semicolon))
+            {
+                var op = Previous;
+                var right = Expression();
+                expr = MakeBinary(op, expr, right);
+            }
+
+            return MakeStatement(NodeKind.Print, printToken, expr);
+        }
+
+        internal Node WhileStatement() 
+        {
+            var token = Previous;
+            var codition = Expression();
+            var body = Block(TokenType.Next);
+
+            return MakeWhile(token, codition, body);
+        }
+
+        internal Node ExpressionStatement()
+        {
+            var expr = Expression();
+            return MakeStatement(NodeKind.Expr, expr.Token, expr);
+        }
+
+        internal Node Declaration()
+        {
+            try
+            {
+                if (Match(TokenType.Let))
+                    return LetDeclaration();
+
+                return Statement();
+            }
+            catch (ParserException e)
+            {
+                Synchronize();
+                return null;
+            }
+        }
+
+        internal Node LetDeclaration()
+        {
+            Consume(TokenType.Identifier, "Expect a identifier for variable.");
+            var name = Previous;
+
+            Node initializer = null;
+            if (Match(TokenType.Equal))
+                initializer = Expression();
+
+            return MakeLet(name, initializer);
+        }
+
+        internal Block Block(TokenType end, TokenType alternativeEnd = TokenType.None)
+        {
+            var token = Previous;
+            var statements = new List<Node>();
+
+            while(!Check(end) && !Check(alternativeEnd) && !Scanner.IsAtEnd())
+                statements.Add(Declaration());
+
+            ConsumeAny(end, alternativeEnd, $"Expect '{end}' after the code block.");
+            return new Block(token, Previous, statements.ToArray());
+        }
+
+        internal Node Expression()
+        {
+            return Assignment();
+        }
+
+        internal Node Assignment()
+        {
+            var expr = Or();
+            
+            if(Match(TokenType.Equal))
+            {
+                var equal = Previous;
+                var right = Assignment();
+
+                if (expr.Type.Kind == NodeKind.Variable)
+                {
+                    return MakeAssign(equal, expr, right);
+                }
+
+                ErrorAt(equal, "Invalid assignment target.");
+            }
+
+            return expr;
+        }
+
+        internal Node Or()
+        {
+            var expr = Xor();
+
+            while(Match(TokenType.Or))
+            {
+                var op = Previous;
+                var right = Xor();
+                expr = MakeLogical(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node Xor()
+        {
+            var expr = And();
+
+            while(Match(TokenType.Xor))
+            {
+                var op = Previous;
+                var right = And();
+                expr = MakeLogical(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node And()
+        {
+            var expr = Equality();
+
+            while (Match(TokenType.And))
+            {
+                var op = Previous;
+                var right = Equality();
+                expr = MakeLogical(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node Equality()
+        {
+            var expr = Comparison();
+
+            while(Match(TokenType.BangEqual, TokenType.EqualEqual))
+            {
+                var op = Previous;
+                var right = Comparison();
+
+                expr = MakeBinary(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node Comparison()
+        {
+            var expr = Term();
+
+            while(Match(TokenType.Greater,
+                TokenType.GreaterEqual, 
+                TokenType.Less, 
+                TokenType.LessEqual))
+            {
+                var op = Previous;
+                var right = Term();
+
+                expr = MakeBinary(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node Term()
+        {
+            var expr = Factor();
+
+            while(Match(TokenType.Minus, TokenType.Plus))
+            {
+                var op = Previous;
+                var right = Factor();
+                
+                expr = MakeBinary(op, expr, right);
+            }
+
+            return expr;
+        }
+
+        internal Node Factor()
+        {
+            var expr = Unary();
+
+            while(Match(TokenType.Slash, TokenType.Star))
+            {
+                var op = Previous;
+                var right = Unary();
+                
+                expr = MakeBinary(op, expr, right);
+            }
+
+            return expr;
+        }
+        internal Node Unary()
+        {
+            if(Match(TokenType.Bang, TokenType.Minus))
+            {
+                var op = Previous;
+                var right = Unary();
+                var type = new NodeType(NodeClass.Expr, NodeKind.Unary);
+                return GetNode(type, op, null, right);
+            }
+
+            return Primary();
+        }
+
+        internal Node Primary()
+        {
+            if (Match(TokenType.False, TokenType.True))
+                return MakeBool(Previous);
+
+            if (Match(TokenType.Null))
+                return MakeNull(Previous);
+
+            if (Match(TokenType.Number))
+                return MakeNumber(Previous);
+
+            if (Match(TokenType.String))
+                return MakeString(Previous);
+
+            if (Match(TokenType.Identifier))
+                return MakeVariable(Previous);
+
+            if(Match(TokenType.LeftParen))
+            {
+                var expr = Expression();
+                Consume(TokenType.RightBracket, "Expect ')' after expression.");
+            }
+
+            throw ErrorAt(Current, "Expect expression");
+        }
+
+        private Node GetNode(NodeType type, Token token, Node childLeft = null, Node childRight = null, Node condition = null)
+        {
+            try
+            {
+                var result = m_pool.Pop();
+                
+                result.Type = type;
+                result.Token = token;
+                result.ChildLeft = childLeft;
+                result.ChildRight = childRight;
+                result.Condition = condition;
+
+                return result;
+            }
+            catch (InvalidOperationException)
+            {
+                return new Node(type, token, childLeft, childRight, condition);
+            }
+        }
+
+        private bool Match(TokenType type1, TokenType type2, TokenType type3, TokenType type4)
+        {
+            if (Check(type1) || Check(type2) || Check(type3) || Check(type4))
+            {
+                Advance();
+                return true;
+            }
+            return false;
+        }
+
+        private bool Match(TokenType type1, TokenType type2)
+        {
+            if (Check(type1) || Check(type2))
+            {
+                Advance();
+                return true;
+            }
+            return false;
+        }
+
+        private bool Match(TokenType type)
+        {
+            if(Check(type))
+            {
+                Advance();
+                return true;
+            }
+            return false;
+        }
+
+        private bool Check(TokenType type)
+        {
+            return m_current.Type == type;
+        }
+
+        private Node MakeStatement(NodeKind kind, Token token, Node args)
+        {
+            var type = new NodeType(NodeClass.Stmt, kind);
+            return GetNode(type, token, args);
+        }
+
+        private Node MakeIf(Token token, Node condition, Node thenBranch, Node elseBranch)
+        {
+            var type = new NodeType(NodeClass.Stmt, NodeKind.If);
+            return GetNode(type, token, thenBranch, elseBranch, condition);
+        }
+
+        private Node MakeLet(Token name, Node initializer)
+        {
+            return MakeStatement(NodeKind.Let, name, initializer);
+        }
+
+        private Node MakeWhile(Token token, Node condition, Node body)
+        {
+            var type = new NodeType(NodeClass.Stmt, NodeKind.While);
+            return GetNode(type, token, body, null, condition);
+        }
+
+        private Node MakeBinary(Token op, Node left, Node right)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Binary);
+            return GetNode(type, op, childLeft: left, childRight: right);
+        }
+
+        private Node MakeLogical(Token op, Node left, Node right)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Logical);
+            return GetNode(type, op, childLeft: left, childRight: right);
+        }
+
+        private Node MakeAssign(Token op, Node left, Node right)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Assign);
+            return GetNode(type, op, left, right);
+        }
+
+        private Node MakeBool(Token token)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Literal, LiteralType.Bool);
+            return GetNode(type, token);
+        }
+
+        private Node MakeNull(Token token)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Literal, LiteralType.Null);
+            return GetNode(type, token);
+        }
+
+        private Node MakeNumber(Token token)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Literal, LiteralType.Number);
+            return GetNode(type, token);
+        }
+
+        private Node MakeString(Token token)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Literal, LiteralType.String);
+            return GetNode(type, token);
+        }
+
+        private Node MakeVariable(Token token)
+        {
+            var type = new NodeType(NodeClass.Expr, NodeKind.Variable);
+            return GetNode(type, token);
+        }
+
+        private Node MakeIncrementStatement(Token mainToken, Token name, Node expr)
+        {
+            var variable = MakeVariable(name);
+            var sum = MakeBinary(new Token(TokenType.Plus, mainToken.Lexeme, mainToken.Line), variable, expr);
+            var assign = MakeAssign(new Token(TokenType.Plus, mainToken.Lexeme, mainToken.Line), variable, sum);
+
+            return MakeStatement(NodeKind.Expr, mainToken, assign);
+        }
+
+        private Node MakeIncrementStatement(Token mainToken, Token name)
+        {
+            var expr = MakeNumber(new Token(TokenType.Number, "1".AsMemory(), mainToken.Line));
+            return MakeIncrementStatement(mainToken, name, expr);
+        }
+
+        private Node MakeLessEqualExpression(Token mainToken, Token name, Node expr)
+        {
+            var variable = MakeVariable(name);
+            var comparison = MakeBinary(new Token(TokenType.LessEqual, mainToken.Lexeme, mainToken.Line), variable, expr);
+            return comparison;
         }
     }
 }
