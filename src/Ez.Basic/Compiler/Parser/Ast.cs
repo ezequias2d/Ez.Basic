ab297e679fa51e16d8120d50019e873361383eba
ezequias2d <ezequiasmoises@gmail.com>
2022-04-03 02:28:35 -0300

    Inprove Ast.Node

--- src/Ez.Basic/Compiler/Parser/Ast.cs
+++ src/Ez.Basic/Compiler/Parser/Ast.cs
@@ -1,12 +1,14 @@
 ﻿using Ez.Basic.Compiler.Lexer;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Ez.Basic.Compiler.Parser
 {
-    internal ref struct Ast
+    internal class Ast
     {
+
         public enum NodeClass
         {
             None,
@@ -62,14 +64,106 @@ namespace Ez.Basic.Compiler.Parser
             public readonly NodeClass Class;
             public readonly NodeKind Kind;
             public readonly LiteralType LiteralType;
+
+            public NodeType(NodeClass nodeClass, NodeKind kind, LiteralType type = LiteralType.None)
+            {
+                Debug.Assert(kind != NodeKind.Literal || type != LiteralType.None);
+                Class = nodeClass;
+                Kind = kind;
+                LiteralType = type; 
+            }
+
+            public override string ToString()
+            {
+                return $"{{{Class}, {Kind}, {LiteralType}}}";
+            }
         }
 
-        public ref struct Node
+        public class Node
         {
             public NodeType Type;
             public Token Token;
-            public Span<int> Children;
-            public int Parent;
+            public Node ChildLeft;
+            public Node ChildRight;
+            public Node Condition;
+
+            public Node(NodeType type, Token token, Node childLeft, Node childRight, Node condition)
+            {
+                Type = type;
+                Token = token;
+                ChildLeft = childLeft;
+                ChildRight = childRight;
+                Condition = condition;
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+
+                sb.Append(" [");
+                if(Type.LiteralType != LiteralType.None)
+                    sb.Append($"{Token.Lexeme}");
+                else
+                {
+                    sb.Append($"{Type.Kind} ");
+
+
+                    if (Token.Type == TokenType.Identifier ||
+                        Type.Kind == NodeKind.Binary ||
+                        Type.Kind == NodeKind.Logical ||
+                        Type.Kind == NodeKind.Unary) 
+                        sb.Append($"{Token.Lexeme} ");
+
+                    if (Condition != null)
+                        sb.Append($"{Condition}");
+
+                    if (ChildLeft != null)
+                        sb.Append($"{ChildLeft}");
+
+
+                    if (ChildRight != null)
+                        sb.Append($"{ChildRight}");
+                }
+
+                if (sb[sb.Length - 1] == '\n')
+                    sb.Remove(sb.Length - 1, 1);
+
+                sb.Append("]");
+
+                return sb.ToString();
+            }
+        }
+
+        public class Block : Node
+        {
+            public ReadOnlyMemory<Node> Statements;
+            public Token EndToken;
+
+            public Block(Token token, Token endToken, ReadOnlyMemory<Node> statements) 
+                : base(new NodeType(NodeClass.Stmt, NodeKind.Block), token, null, null, null)
+            {
+                Statements = statements;
+                EndToken = endToken;
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                sb.Append($"{Token.Lexeme} \n");
+
+                foreach(var stmt in Statements.Span)
+                {
+                    sb.Append("\t");
+                    if (stmt != null)
+                        sb.AppendLine(stmt.ToString().Replace("\t", "\t\t"));
+                    else
+                        sb.AppendLine("NULL");
+                }
+
+                sb.Append($"\n{EndToken}");
+
+                return sb.ToString();
+            }
         }
     }
 }
